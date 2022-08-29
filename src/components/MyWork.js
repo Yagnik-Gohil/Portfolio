c27309f84ee05faff59452bcb131cf5039028553
Yagnik <59044382+Yagnik-Gohil@users.noreply.github.com>
2022-08-29 15:29:10 +0530

    Added Resume Builder

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -143,6 +143,39 @@ function MyWork() {
                             </div>
                         
                     </div>
+                    <div className="col-md-4 text-center my-2">
+                        
+                        <div id="carouselExampleControls5" className="carousel slide my-1" data-bs-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                <img src="../portfolio-images/resume-1.jpg" className="d-block w-100" alt="..."/>
+                                </div>
+                                <div className="carousel-item">
+                                <img src="../portfolio-images/resume-2.jpg" className="d-block w-100" alt="..."/>
+                                </div>
+                                <div className="carousel-item">
+                                <img src="../portfolio-images/resume-3.jpg" className="d-block w-100" alt="..."/>
+                                </div>
+                                <div className="carousel-item">
+                                <img src="../portfolio-images/resume-4.jpg" className="d-block w-100" alt="..."/>
+                                </div>
+                            </div>
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls5" data-bs-slide="prev">
+                                <GrPrevious/>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls5" data-bs-slide="next">
+                                <GrNext/>
+                                <span className="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                            <h5 className="ff-jose my-1">Resume Builder</h5>
+                            <div className="row d-flex justify-content-center">
+                                <a href="https://resume-builder-yg.netlify.app/" className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded" target="_blank"><BsLaptop/> Watch Live</a>
+                                <a href="https://github.com/Yagnik-Gohil/Resume-Builder" className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded" target="_blank"><AiFillGithub/> Source Code</a>
+                            </div>
+                        
+                    </div>
                 </div>
                 
             </div>
